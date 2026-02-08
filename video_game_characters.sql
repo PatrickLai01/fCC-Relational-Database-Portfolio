fcb0e366cd8ae7bad78d140b10d334dc3bc67e90
PatrickLai01 <patjlai@gmail.com>
2026-02-07 17:02:50 -0800

    Add files via upload

---
+++ video_game_characters.sql
@@ -0,0 +1,434 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
+-- Dumped by pg_dump version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+ALTER TABLE IF EXISTS ONLY public.sounds DROP CONSTRAINT IF EXISTS sounds_character_id_fkey;
+ALTER TABLE IF EXISTS ONLY public.more_info DROP CONSTRAINT IF EXISTS more_info_character_id_fkey;
+ALTER TABLE IF EXISTS ONLY public.character_actions DROP CONSTRAINT IF EXISTS character_actions_character_id_fkey;
+ALTER TABLE IF EXISTS ONLY public.character_actions DROP CONSTRAINT IF EXISTS character_actions_action_id_fkey;
+ALTER TABLE IF EXISTS ONLY public.sounds DROP CONSTRAINT IF EXISTS sounds_pkey;
+ALTER TABLE IF EXISTS ONLY public.sounds DROP CONSTRAINT IF EXISTS sounds_filename_key;
+ALTER TABLE IF EXISTS ONLY public.more_info DROP CONSTRAINT IF EXISTS more_info_pkey;
+ALTER TABLE IF EXISTS ONLY public.more_info DROP CONSTRAINT IF EXISTS more_info_more_info_id_key;
+ALTER TABLE IF EXISTS ONLY public.more_info DROP CONSTRAINT IF EXISTS more_info_character_id_key;
+ALTER TABLE IF EXISTS ONLY public.characters DROP CONSTRAINT IF EXISTS characters_pkey;
+ALTER TABLE IF EXISTS ONLY public.character_actions DROP CONSTRAINT IF EXISTS character_actions_pkey;
+ALTER TABLE IF EXISTS ONLY public.actions DROP CONSTRAINT IF EXISTS actions_pkey;
+ALTER TABLE IF EXISTS ONLY public.actions DROP CONSTRAINT IF EXISTS actions_action_key;
+ALTER TABLE IF EXISTS public.sounds ALTER COLUMN sound_id DROP DEFAULT;
+ALTER TABLE IF EXISTS public.more_info ALTER COLUMN more_info_id DROP DEFAULT;
+ALTER TABLE IF EXISTS public.characters ALTER COLUMN character_id DROP DEFAULT;
+ALTER TABLE IF EXISTS public.actions ALTER COLUMN action_id DROP DEFAULT;
+DROP SEQUENCE IF EXISTS public.sounds_sound_id_seq;
+DROP TABLE IF EXISTS public.sounds;
+DROP SEQUENCE IF EXISTS public.more_info_more_info_id_seq;
+DROP TABLE IF EXISTS public.more_info;
+DROP SEQUENCE IF EXISTS public.characters_character_id_seq;
+DROP TABLE IF EXISTS public.characters;
+DROP TABLE IF EXISTS public.character_actions;
+DROP SEQUENCE IF EXISTS public.actions_action_id_seq;
+DROP TABLE IF EXISTS public.actions;
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: actions; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.actions (
+    action_id integer NOT NULL,
+    action character varying(20) NOT NULL
+);
+
+
+--
+-- Name: actions_action_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.actions_action_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: actions_action_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.actions_action_id_seq OWNED BY public.actions.action_id;
+
+
+--
+-- Name: character_actions; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.character_actions (
+    character_id integer NOT NULL,
+    action_id integer NOT NULL
+);
+
+
+--
+-- Name: characters; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.characters (
+    character_id integer NOT NULL,
+    name character varying(30) NOT NULL,
+    homeland character varying(60),
+    favorite_color character varying(30)
+);
+
+
+--
+-- Name: characters_character_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.characters_character_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: characters_character_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.characters_character_id_seq OWNED BY public.characters.character_id;
+
+
+--
+-- Name: more_info; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.more_info (
+    more_info_id integer NOT NULL,
+    birthday date,
+    height_in_cm integer,
+    weight_in_kg numeric(4,1),
+    character_id integer NOT NULL
+);
+
+
+--
+-- Name: more_info_more_info_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.more_info_more_info_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: more_info_more_info_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.more_info_more_info_id_seq OWNED BY public.more_info.more_info_id;
+
+
+--
+-- Name: sounds; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.sounds (
+    sound_id integer NOT NULL,
+    filename character varying(40) NOT NULL,
+    character_id integer NOT NULL
+);
+
+
+--
+-- Name: sounds_sound_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.sounds_sound_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: sounds_sound_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.sounds_sound_id_seq OWNED BY public.sounds.sound_id;
+
+
+--
+-- Name: actions action_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.actions ALTER COLUMN action_id SET DEFAULT nextval('public.actions_action_id_seq'::regclass);
+
+
+--
+-- Name: characters character_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.characters ALTER COLUMN character_id SET DEFAULT nextval('public.characters_character_id_seq'::regclass);
+
+
+--
+-- Name: more_info more_info_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.more_info ALTER COLUMN more_info_id SET DEFAULT nextval('public.more_info_more_info_id_seq'::regclass);
+
+
+--
+-- Name: sounds sound_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sounds ALTER COLUMN sound_id SET DEFAULT nextval('public.sounds_sound_id_seq'::regclass);
+
+
+--
+-- Data for Name: actions; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.actions (action_id, action) FROM stdin;
+1	run
+2	jump
+3	duck
+\.
+
+
+--
+-- Data for Name: character_actions; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.character_actions (character_id, action_id) FROM stdin;
+7	1
+7	2
+7	3
+6	1
+6	2
+6	3
+5	1
+5	2
+5	3
+4	1
+4	2
+4	3
+3	1
+3	2
+3	3
+2	1
+2	2
+2	3
+1	1
+1	2
+1	3
+\.
+
+
+--
+-- Data for Name: characters; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.characters (character_id, name, homeland, favorite_color) FROM stdin;
+2	Luigi	Mushroom Kingdom	Green
+3	Peach	Mushroom Kingdom	Pink
+7	Yoshi	Dinosaur Land	Green
+6	Daisy	Sarasaland	Orange
+1	Mario	Mushroom Kingdom	Red
+4	Toad	Mushroom Kingdom	Blue
+5	Bowser	Koopa Kingdom	Yellow
+\.
+
+
+--
+-- Data for Name: more_info; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.more_info (more_info_id, birthday, height_in_cm, weight_in_kg, character_id) FROM stdin;
+1	1981-07-09	155	64.5	1
+2	1983-07-14	175	48.8	2
+3	1985-10-18	173	52.2	3
+4	1950-01-10	66	35.6	4
+5	1990-10-29	258	300.0	5
+6	1989-07-31	\N	\N	6
+7	1990-04-13	162	59.1	7
+\.
+
+
+--
+-- Data for Name: sounds; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.sounds (sound_id, filename, character_id) FROM stdin;
+1	its-a-me.wav	1
+2	yippee.wav	1
+4	ha-ha.wav	2
+5	oh-yeah.wav	2
+6	yay.wav	3
+7	woo-hoo.wav	3
+8	mm-hmm.wav	3
+9	yahoo.wav	1
+\.
+
+
+--
+-- Name: actions_action_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.actions_action_id_seq', 3, true);
+
+
+--
+-- Name: characters_character_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.characters_character_id_seq', 7, true);
+
+
+--
+-- Name: more_info_more_info_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.more_info_more_info_id_seq', 7, true);
+
+
+--
+-- Name: sounds_sound_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.sounds_sound_id_seq', 9, true);
+
+
+--
+-- Name: actions actions_action_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.actions
+    ADD CONSTRAINT actions_action_key UNIQUE (action);
+
+
+--
+-- Name: actions actions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.actions
+    ADD CONSTRAINT actions_pkey PRIMARY KEY (action_id);
+
+
+--
+-- Name: character_actions character_actions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.character_actions
+    ADD CONSTRAINT character_actions_pkey PRIMARY KEY (character_id, action_id);
+
+
+--
+-- Name: characters characters_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.characters
+    ADD CONSTRAINT characters_pkey PRIMARY KEY (character_id);
+
+
+--
+-- Name: more_info more_info_character_id_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.more_info
+    ADD CONSTRAINT more_info_character_id_key UNIQUE (character_id);
+
+
+--
+-- Name: more_info more_info_more_info_id_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.more_info
+    ADD CONSTRAINT more_info_more_info_id_key UNIQUE (more_info_id);
+
+
+--
+-- Name: more_info more_info_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.more_info
+    ADD CONSTRAINT more_info_pkey PRIMARY KEY (more_info_id);
+
+
+--
+-- Name: sounds sounds_filename_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sounds
+    ADD CONSTRAINT sounds_filename_key UNIQUE (filename);
+
+
+--
+-- Name: sounds sounds_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sounds
+    ADD CONSTRAINT sounds_pkey PRIMARY KEY (sound_id);
+
+
+--
+-- Name: character_actions character_actions_action_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.character_actions
+    ADD CONSTRAINT character_actions_action_id_fkey FOREIGN KEY (action_id) REFERENCES public.actions(action_id);
+
+
+--
+-- Name: character_actions character_actions_character_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.character_actions
+    ADD CONSTRAINT character_actions_character_id_fkey FOREIGN KEY (character_id) REFERENCES public.characters(character_id);
+
+
+--
+-- Name: more_info more_info_character_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.more_info
+    ADD CONSTRAINT more_info_character_id_fkey FOREIGN KEY (character_id) REFERENCES public.characters(character_id);
+
+
+--
+-- Name: sounds sounds_character_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sounds
+    ADD CONSTRAINT sounds_character_id_fkey FOREIGN KEY (character_id) REFERENCES public.characters(character_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
